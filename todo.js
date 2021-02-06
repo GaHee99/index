c19c695d0e9d51d0f74bad2c6196eddaebc71d11
GaHee99 <77230391+GaHee99@users.noreply.github.com>
2021-02-06 19:08:33 +0900

    todo..so hard

--- todo.js
+++ todo.js
@@ -4,8 +4,22 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
   const TODOS_LS = "toDos";
   
-  const toDos=[];
+  let toDos=[];
 //toDos를 갖고와서 로컬에 저장 
+
+
+  function deleteToDo(event){
+      const btn = event.target;
+      const li = btn.parentNode;
+      toDoList.removeChild(li);
+      const cleanToDos= toDos.filter(function(toDo){
+        return toDo.id !== parseInt(li.id);
+      });
+     toDos=cleanToDos;
+     saveToDos();
+  }
+
+
 function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
@@ -19,6 +33,7 @@ function saveToDos() {
     const span = document.createElement("span");
     const newId = toDos.length + 1;
     delBtn.innerText = "❌";
+    delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
